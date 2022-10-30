a5f5b6e03190f1c812fa93666e6f5b71cb3f1e4a
Khue Lee <khuelee55@gmail.com>
2022-10-30 16:51:09 -0500

    created collection variable, music1 object, and function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,16 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+    collection.push(music1);
+    return collection;
+}
+
+let music1 = {
+    title: '',
+    artist: '',
+    yearPublished: '',
+}
+
+console.log(addToCollection());
