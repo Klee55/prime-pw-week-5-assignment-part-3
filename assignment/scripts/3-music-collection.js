e621e40ba3aacedb0822245802b07e227e7ab48f
Khue Lee <khuelee55@gmail.com>
2022-10-30 17:47:07 -0500

    fixed function to add albums to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,17 +3,34 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection(title, artist, yearPublished){
-    music1.title = title;
-    music1.artist = artist;
-    music1.yearPublished = yearPublished;
-    collection.push(music1);
+    let musiclist = {
+        title:'',
+        artist:'',
+        yearPublished:'',
+    }
+    musiclist.title = title;
+    musiclist.artist = artist;
+    musiclist.yearPublished = yearPublished;
+    collection.push(musiclist);
     return collection;
+    
 }
 
-let music1 = {
-    title: '',
-    artist: '',
-    yearPublished: '',
-}
 
-console.log(addToCollection());
+//console.log(addToCollection());
+
+addToCollection('You Are Beautiful', 'James Blunt', 2004);
+addToCollection('Girls Like You', 'Maroon 5', 2017);
+//console.log(collection);
+addToCollection('Shape Of You', 'Ed Sheeran', 2017);
+addToCollection('Talking To The Moon', 'Bruno Mars', 2010);
+addToCollection('Not Afraid', 'Eminem', 2010);
+
+
+
+
+
+
+//music1.title = 'Lee';
+//music1.title = 'Awesome';
+
